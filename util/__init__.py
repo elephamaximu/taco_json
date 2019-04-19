889d8ede033d0467897bba415213c6efe21ffd6f
정인창 <elephamaximu@gmail.com>
2019-04-19 14:55:05 +0900

    util.__init__.py 내용 추가

--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,6 @@
+from multiprocessing import Pool
+from tqdm import tqdm
+
 class ValueWindow():
   def __init__(self, window_size=100):
     self._window_size = window_size
@@ -20,3 +23,24 @@ class ValueWindow():
 
   def reset(self):
     self._values = []
+
+def parallel_run(fn, items, desc="", parallel=True):
+  results = []
+
+  if parallel:
+    with closing(Pool()) as pool:
+      for out in tqdm(pool.imap_unordered(
+        fn, items), total=len(items), desc=desc):
+        if out is not None:
+          results.append(out)
+  else:
+    for item in tqdm(items, total=len(items), desc=desc):
+      out = fn(item)
+      if out is not None:
+        results.append(out)
+  
+  return results
+
+  def add_postfix(path, postfix):
+    path_without_ext, ext = path.rsplit('.', 1)
+    return "{}.{}.{}".format(path_without_ext, postfix, ext)
